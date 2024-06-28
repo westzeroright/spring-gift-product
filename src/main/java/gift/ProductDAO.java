f69ed9787b9f2e7e34395c197e2ec842a5911da8
SeoYeongU <westzeroright@gmail.com>
2024-06-28 16:48:13 +0900

    feat(ProductDAO): JdbcClient를 사용하는 레포지토리 구현

---
+++ src/main/java/gift/ProductDAO.java
@@ -0,0 +1,91 @@
+
+package gift;
+
+import java.util.ArrayList;
+import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class ProductDAO {
+    private final JdbcClient jdbcClient;
+
+    public ProductDAO(JdbcClient jdbcClient) {
+        this.jdbcClient = jdbcClient;
+    }
+
+    public void createProductTable() {
+        var sql = """
+            create table product(
+                id long not null,
+                name varchar(255),
+                price int,
+                url varchar(255),
+                primary key (id)
+            )
+            """;
+        jdbcClient.sql(sql).update();
+    }
+
+    public void insertProduct(Product product) {
+        var sql = "insert into product (id, name, price, url) values (?, ?, ?, ?)";
+        jdbcClient.sql(sql)
+            .param(product.getId())
+            .param(product.getName())
+            .param(product.getPrice())
+            .param(product.getUrl())
+            .update();
+    }
+
+    public Product selectProduct(Long id) {
+        var sql = "select id, name, price, url from product where id = ?";
+        return jdbcClient.sql(sql)
+            .param(id)
+            .query(resultSet -> {
+                if (resultSet.next()) {
+                    return new Product(
+                        //resultSet.getLong("id"),
+                        resultSet.getString("name"),
+                        resultSet.getInt("price"),
+                        resultSet.getString("url")
+                    );
+                }
+                return null;
+            });
+    }
+
+    public List<Product> findAllProducts() {
+        var sql = "select id, name, price, url from product";
+        return jdbcClient.sql(sql)
+            .query(resultSet -> {
+                List<Product> products = new ArrayList<>();
+                while (resultSet.next()) {
+                    products.add(new Product(
+                        //resultSet.getLong("id"),
+                        resultSet.getString("name"),
+                        resultSet.getInt("price"),
+                        resultSet.getString("url")
+                    ));
+                }
+                return products;
+            });
+    }
+
+    public void updateProduct(Product product) {
+        var sql = "update product set name = ?, price = ?, url = ? where id = ?";
+        jdbcClient.sql(sql)
+            .param(product.getName())
+            .param(product.getPrice())
+            .param(product.getUrl())
+            .param(product.getId())
+            .update();
+    }
+
+    public void deleteProduct(Long id) {
+        var sql = "delete from product where id = ?";
+        jdbcClient.sql(sql)
+            .param(id)
+            .update();
+    }
+}
