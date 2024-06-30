df781779be9704f94480f693fc14dfc223d937f5
SeoYeongU <westzeroright@gmail.com>
2024-06-30 22:02:33 +0900

    fix(ProductDao): 버그 수정

--- src/main/java/gift/ProductDao.java
+++ src/main/java/gift/ProductDao.java
@@ -1,96 +1,59 @@
-
 package gift;
 
-import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public class ProductDao {
-    private final JdbcClient jdbcClient;
+    @Autowired
+    private JdbcClient jdbcClient;
 
-    public ProductDao(JdbcClient jdbcClient) {
-        this.jdbcClient = jdbcClient;
-    }
+    public ProductDao(JdbcClient jdbcClient) {this.jdbcClient = jdbcClient;}
 
-    public void createProductTable() {
-        var sql = """
-            create table product(
-                id long not null,
-                name varchar(255),
-                price int,
-                url varchar(255),
-                primary key (id)
-            )
-            """;
-        jdbcClient.sql(sql).update();
-    }
-
-    public void insertProduct(Product product) {
-        var sql = "insert into product (id, name, price, url) "
-            + "values (?, ?, ?, ?)";
-        jdbcClient.sql(sql)
-            .param(product.getId())
-            .param(product.getName())
-            .param(product.getPrice())
-            .param(product.getUrl())
-            .update();
-    }
-
-    public Product selectProduct(Long id) {
-        var sql = "select id, name, price, url "
-            + "from product "
-            + "where id = ?";
-        return jdbcClient.sql(sql)
-            .param(id)
-            .query(resultSet -> {
-                if (resultSet.next()) {
-                    return new Product(
-                        //resultSet.getLong("id"),
-                        resultSet.getString("name"),
-                        resultSet.getInt("price"),
-                        resultSet.getString("url")
-                    );
-                }
-                return null;
-            });
-    }
-
-    public List<Product> findAllProducts() {
-        var sql = "select id, name, price, url "
+    public List<Product> findAllProduct() {
+        var sql = "select *"
             + "from product";
-        return jdbcClient.sql(sql)
-            .query(resultSet -> {
-                List<Product> products = new ArrayList<>();
-                while (resultSet.next()) {
-                    products.add(new Product(
-                        //resultSet.getLong("id"),
-                        resultSet.getString("name"),
-                        resultSet.getInt("price"),
-                        resultSet.getString("url")
-                    ));
-                }
-                return products;
-            });
+        List<Product> products = jdbcClient.sql(sql).query(Product.class).list();
+        return products;
     }
 
-    public void updateProduct(Product product) {
-        var sql = "update product "
-            + "set name = ?, price = ?, url = ? "
-            + "where id = ?";
+    public Optional<Product> findProductById(Long id) {
+        var sql = "select id, name, price, url"
+            + " from product"
+            + " where id = ?";
+        return jdbcClient.sql(sql).param(id).query(Product.class).optional();
+    }
+
+    public void addProduct(Product product) {
+        var sql = "insert into product (id, name, price, url)"
+            + "values (?,?,?,?)";
+
         jdbcClient.sql(sql)
+            .param(product.getId())
             .param(product.getName())
             .param(product.getPrice())
             .param(product.getUrl())
-            .param(product.getId())
             .update();
     }
 
-    public void deleteProduct(Long id) {
-        var sql = "delete from product "
+    public Integer updateProductById(Long id, ProductRequestDto productRequestDto) {
+        var sql = "update product "
+            + "set name = ?, price = ?, url = ?"
             + "where id = ?";
+
+        return jdbcClient.sql(sql)
+            .param(productRequestDto.name())
+            .param(productRequestDto.price())
+            .param(productRequestDto.url())
+            .param(id)
+            .update();
+    }
+    public void deleteProductById(Long id) {
+        var sql = "delete from product"
+            + " where id = ?";
         jdbcClient.sql(sql)
             .param(id)
             .update();
